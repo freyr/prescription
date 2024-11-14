126acb2212f4690448601331d188a9d8cb0b1800
Michal Giergielewicz <michal@giergielewicz.pl>
2024-11-14 09:03:29 +0100

    phpunit

---
+++ tests/Issuing/Core/Prescription/PrescriptionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Freyr\Prescription\Tests\Issuing\Core\Prescription;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class PrescriptionTest extends TestCase
+{
+
+    #[test]
+    public function issuing(): void
+    {
+
+    }
+}
